356898a9a2fb9e3c5f2809dcc65eca33db1e85f4
Irshad Ali <irshadali786143@gmail.com>
2020-01-25 17:01:45 +0530

    Port Adds Chnaged

--- sss-persistance/src/app.py
+++ sss-persistance/src/app.py
@@ -47,4 +47,4 @@ def createOrUpdateSSS():
     return jsonify({'result': bson_dumps(result)})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='localhost', port=5000) #removed 127.0.0.1
--- sss-renderer/src/app/conf.py
+++ sss-renderer/src/app/conf.py
@@ -4,16 +4,16 @@ DATABASE = ('sss-dsl',) # Database connection
 USERS_COLLECTION= ['user-info'] # users info collection
 
 # To run the app on local machine
-HOST = ['127.0.0.1']
+HOST = ['localhost'] # Removed 127.0.0.1
 PORT = [8090]
 # Comment above lines and uncomment below lines to run the app on server 
 # HOST = ['0.0.0.0']
 # PORT = [8090]
 
 # SSS-DB address (To modify the host name to server address change hostname in 'SSS_DB_HOST_ADRS')  
-SSS_DB_HOST_ADRS = ['127.0.0.1']
+SSS_DB_HOST_ADRS = ['localhost'] # Removed 127.0.0.1
 SSS_DB_PORT_ADRS = [5000]
 
 # Web transformation engine API address (To modify the host name to server address change hostname in 'XFORM_API_HOST_ADRS')
-XFORM_API_HOST_ADRS = ['127.0.0.1']
+XFORM_API_HOST_ADRS = ['localhost'] # Removed 127.0.0.1
 XFORM_API_PORT_ADRS = [8006]
