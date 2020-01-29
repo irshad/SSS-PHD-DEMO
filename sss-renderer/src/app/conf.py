ab5274f7039e62a0c9bc05095321dc16621d266d
Irshad Ali <irshadali786143@gmail.com>
2020-01-29 09:57:21 +0530

    localhost

--- sss-renderer/src/app/conf.py
+++ sss-renderer/src/app/conf.py
@@ -4,11 +4,11 @@ DATABASE = ('sss-dsl',) # Database connection
 USERS_COLLECTION= ['user-info'] # users info collection
 
 # To run the app on local machine
-HOST = ['localhost'] # Removed 127.0.0.1
-PORT = [8090]
-# Comment above lines and uncomment below lines to run the app on server 
-# HOST = ['0.0.0.0']
+# HOST = ['localhost'] # Removed 127.0.0.1
 # PORT = [8090]
+# Comment above lines and uncomment below lines to run the app on server 
+HOST = ['0.0.0.0']
+PORT = [8090]
 
 # SSS-DB address (To modify the host name to server address change hostname in 'SSS_DB_HOST_ADRS')  
 SSS_DB_HOST_ADRS = ['localhost'] # Removed 127.0.0.1
--- sss-renderer/src/webxfrmtn-engine/conf.py
+++ sss-renderer/src/webxfrmtn-engine/conf.py
@@ -3,8 +3,8 @@ DEBUG = [True]
 CHROME_LOCATION = ['/usr/bin/google-chrome',] # google-chrome binary location
 
 # To run the web transformation engine API on local machine
-XFRM_API_HOST = ['localhost'] # Removed 127.0.0.1
-XFRM_API_PORT = [8006]
-# Comment above lines and uncomment below lines to run the web transformation engine API on server 
-# XFRM_API_HOST = ['0.0.0.0']
+# XFRM_API_HOST = ['localhost'] # Removed 127.0.0.1
 # XFRM_API_PORT = [8006]
+# Comment above lines and uncomment below lines to run the web transformation engine API on server 
+XFRM_API_HOST = ['0.0.0.0']
+XFRM_API_PORT = [8006]
